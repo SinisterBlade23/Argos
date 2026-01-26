f2bddc58ab243e5405f9362b2d31b62abb5b106d
SinisterBlade23 <kapoorrushil11@gmail.com>
2026-01-26 13:58:13 +0530

    added ros2_control interface and changed pkg name

--- my_robot_bringup/launch/display.launch.py
+++ my_robot_bringup/launch/display.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
 
-    urdf_path = os.path.join(get_package_share_path('my_robot_description'),
+    urdf_path = os.path.join(get_package_share_path('my_robot_bringup'),
                              'urdf', 'SimAssemb.urdf.xacro')
     
     robot_description = ParameterValue(Command(['xacro ',urdf_path ]),value_type=str )
